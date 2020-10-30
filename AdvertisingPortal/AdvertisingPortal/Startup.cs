2c0a03f15158d8c716dde1e95d02992328054e88
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-10-30 14:36:17 +0100

    Add RazorPages and Authentication in Startup.cs

--- AdvertisingPortal/AdvertisingPortal/Startup.cs
+++ AdvertisingPortal/AdvertisingPortal/Startup.cs
@@ -24,6 +24,7 @@ namespace AdvertisingPortal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,7 @@ namespace AdvertisingPortal
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +53,7 @@ namespace AdvertisingPortal
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
