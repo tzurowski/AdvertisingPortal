23b2fa134e193c05454551223f7c607d8996fae1
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-10-30 14:34:57 +0100

    Add Identity (Login and Register)

---
+++ AdvertisingPortal/AdvertisingPortal/Areas/Identity/Data/AdvertisingPortalDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertisingPortal.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvertisingPortal.Data
+{
+    public class AdvertisingPortalDbContext : IdentityDbContext<AdvertisingPortalUser>
+    {
+        public AdvertisingPortalDbContext(DbContextOptions<AdvertisingPortalDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ AdvertisingPortal/AdvertisingPortal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AdvertisingPortal.Areas.Identity.Data;
+using AdvertisingPortal.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AdvertisingPortal.Areas.Identity.IdentityHostingStartup))]
+namespace AdvertisingPortal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AdvertisingPortalDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AdvertisingPortalDbContextConnection")));
+
+                services.AddDefaultIdentity<AdvertisingPortalUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AdvertisingPortalDbContext>();
+            });
+        }
+    }
+}
