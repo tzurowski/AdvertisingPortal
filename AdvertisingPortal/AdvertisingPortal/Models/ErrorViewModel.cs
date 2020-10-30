23c982436282b94f153808c15dd284bcf814f3c9
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-10-30 14:29:33 +0100

    Add project files

---
+++ AdvertisingPortal/AdvertisingPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdvertisingPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
