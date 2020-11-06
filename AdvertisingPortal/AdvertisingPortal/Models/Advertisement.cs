700d29675a2761300b2198665dae46ccdae65900
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-06 15:34:29 +0100

    Add Advertisement model

---
+++ AdvertisingPortal/AdvertisingPortal/Models/Advertisement.cs
@@ -0,0 +1,28 @@
+﻿using AdvertisingPortal.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertisingPortal.Models
+{
+    public class Advertisement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AdvertisementId { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string Discription { get; set; }
+        public Car Car { get; set; }
+        public AdvertisingPortalUser User { get; set; }
+    }
+}
