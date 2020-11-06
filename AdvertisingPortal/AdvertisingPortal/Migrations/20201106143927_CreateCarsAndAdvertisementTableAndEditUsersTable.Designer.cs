9f700c65a8c05c8af707cf2f9ea5992171a22d7c
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-06 15:41:58 +0100

    Add CreateCarsAndAdvertisementTableAndEditUsersTable Migration

--- AdvertisingPortal/AdvertisingPortal/Migrations/20201106143927_CreateCarsAndAdvertisementTableAndEditUsersTable.Designer.cs
+++ AdvertisingPortal/AdvertisingPortal/Migrations/20201106143927_CreateCarsAndAdvertisementTableAndEditUsersTable.Designer.cs
@@ -4,14 +4,16 @@ using AdvertisingPortal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdvertisingPortal.Migrations
 {
     [DbContext(typeof(AdvertisingPortalDbContext))]
-    partial class AdvertisingPortalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201106143927_CreateCarsAndAdvertisementTableAndEditUsersTable")]
+    partial class CreateCarsAndAdvertisementTableAndEditUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AdvertisingPortal/AdvertisingPortal/Migrations/20201106143927_CreateCarsAndAdvertisementTableAndEditUsersTable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvertisingPortal.Migrations
+{
+    public partial class CreateCarsAndAdvertisementTableAndEditUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Make = table.Column<string>(nullable: false),
+                    Model = table.Column<string>(nullable: true),
+                    Year = table.Column<DateTime>(nullable: false),
+                    Mileage = table.Column<int>(nullable: false),
+                    FuelTank = table.Column<float>(nullable: false),
+                    FuelType = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.CarId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advertisements",
+                columns: table => new
+                {
+                    AdvertisementId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    Discription = table.Column<string>(nullable: true),
+                    CarId = table.Column<int>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advertisements", x => x.AdvertisementId);
+                    table.ForeignKey(
+                        name: "FK_Advertisements_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "CarId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Advertisements_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisements_CarId",
+                table: "Advertisements",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Advertisements_UserId",
+                table: "Advertisements",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Advertisements");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
