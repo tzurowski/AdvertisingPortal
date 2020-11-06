4df67111d74a70c0cd04d36093e9ca7466754ed3
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-06 15:37:44 +0100

    Add Cars and Advertisements Table to AdvertisingPortalDbContext

--- AdvertisingPortal/AdvertisingPortal/Areas/Identity/Data/AdvertisingPortalDbContext.cs
+++ AdvertisingPortal/AdvertisingPortal/Areas/Identity/Data/AdvertisingPortalDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AdvertisingPortal.Areas.Identity.Data;
+using AdvertisingPortal.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,8 @@ namespace AdvertisingPortal.Data
 {
     public class AdvertisingPortalDbContext : IdentityDbContext<AdvertisingPortalUser>
     {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Advertisement> Advertisements { get; set; }
         public AdvertisingPortalDbContext(DbContextOptions<AdvertisingPortalDbContext> options)
             : base(options)
         {
