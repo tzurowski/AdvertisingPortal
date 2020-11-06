29f6da17a1266ad103299952dcee2d4190e43f17
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-06 15:34:08 +0100

    Add Car model

---
+++ AdvertisingPortal/AdvertisingPortal/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿using AdvertisingPortal.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertisingPortal.Models
+{
+    public class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CarId { get; set; }
+
+        [Required]
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+
+        [Required]
+        public int Mileage { get; set; }
+
+        public float FuelTank { get; set; }
+        public string FuelType { get; set; }
+
+    }
+}
