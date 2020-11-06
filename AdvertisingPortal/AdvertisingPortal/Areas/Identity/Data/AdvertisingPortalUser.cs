0e27905bd632223d7c0194a8ef2070e42432cc7b
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-06 15:33:30 +0100

    Add Advertisements to User

--- AdvertisingPortal/AdvertisingPortal/Areas/Identity/Data/AdvertisingPortalUser.cs
+++ AdvertisingPortal/AdvertisingPortal/Areas/Identity/Data/AdvertisingPortalUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using AdvertisingPortal.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace AdvertisingPortal.Areas.Identity.Data
@@ -17,5 +18,8 @@ namespace AdvertisingPortal.Areas.Identity.Data
         [PersonalData]
         [Column(TypeName = "nvarchar(50)")]
         public string LastName { get; set; }
+
+        [PersonalData]
+        public ICollection<Advertisement> Advertisements { get; set; }
     }
 }
