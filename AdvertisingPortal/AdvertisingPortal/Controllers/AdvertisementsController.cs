05adbe41dea7e18ef0ac3d74ff9a199625abd77e
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-06 16:31:41 +0100

    Add db to controller and data from db to Index page

--- AdvertisingPortal/AdvertisingPortal/Controllers/AdvertisementsController.cs
+++ AdvertisingPortal/AdvertisingPortal/Controllers/AdvertisementsController.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AdvertisingPortal.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdvertisingPortal.Controllers
 {
     public class AdvertisementsController : Controller
     {
+        private readonly AdvertisingPortalDbContext _db;
+        public AdvertisementsController(AdvertisingPortalDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var ads = _db.Advertisements.Select(c => c).ToList();
+            return View(ads);
         }
     }
 }
