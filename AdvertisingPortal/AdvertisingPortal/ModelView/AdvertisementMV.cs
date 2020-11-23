2f2bd02a32c8111ca92f19b3de0a1b7132b2501c
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-23 22:45:27 +0100

    Add ModelView for Display Advertisements

--- AdvertisingPortal/AdvertisingPortal/ModelView/AdvertisementMV.cs
+++ AdvertisingPortal/AdvertisingPortal/ModelView/AdvertisementMV.cs
@@ -7,15 +7,14 @@ namespace AdvertisingPortal.ModelView
 {
     public class AdvertisementMV
     {
+        public string ImageUrl { get; set; }
         public string Title { get; set; }
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
-        public string Discription { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public DateTime Year { get; set; }
-        public int Mileage { get; set; }
         public float FuelTank { get; set; }
         public string FuelType { get; set; }
+        public string Description { get; set; }
     }
 }
