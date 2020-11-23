185c800431ff5762d736491fecaa694b9212e2a7
Tomasz Żurowski <tzurowski9317@gmail.com>
2020-11-23 22:47:34 +0100

    Change model from Advertisement to AdvertisementMV for Index Action

--- AdvertisingPortal/AdvertisingPortal/Controllers/AdvertisementsController.cs
+++ AdvertisingPortal/AdvertisingPortal/Controllers/AdvertisementsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AdvertisingPortal.Data;
+using AdvertisingPortal.ModelView;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using X.PagedList;
@@ -20,7 +21,18 @@ namespace AdvertisingPortal.Controllers
         }
         public IActionResult Index(string search, int? i)
         {
-            var ads = _db.Advertisements.Include(c =>c.Car).Where(x => x.Title.Contains(search) || search == null).ToList().ToPagedList(i ?? 1, 2);
+            var ads = _db.Advertisements.Include(c => c.Car).Where(x => x.Title.Contains(search) || search == null)
+                .Select(x => new AdvertisementMV {
+                    ImageUrl = x.ImageUrl,
+                    Title = x.Title,
+                    Price = x.Price,
+                    Make = x.Car.Make,
+                    Model = x.Car.Model,
+                    Year = x.Car.Year,
+                    FuelTank = x.Car.FuelTank,
+                    FuelType = x.Car.FuelType,
+                    Description = x.Description
+                }).ToList().ToPagedList(i ?? 1, 6);
             return View(ads);
         }
     }
